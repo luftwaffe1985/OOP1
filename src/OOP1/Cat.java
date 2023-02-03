aa1e1596b05781eeb029eb198f0cf8dcf30855bc
luftwaffe1985 <putik1985@yandex.ru>
2023-02-03 16:55:39 +0300

    New commit

---
+++ src/OOP1/Cat.java
@@ -0,0 +1,21 @@
+package OOP1;
+
+public class Cat {
+    String nickname;
+
+    public Cat(String nickname) {
+
+        this.nickname = nickname;
+    }
+
+    public void asksForFood(Human name) {
+        System.out.printf("%s просит кушать у своей хозяйки %s", this.nickname, name);
+        System.out.printf("%s идет кормить %s", name, this.nickname);
+    }
+
+    @Override
+    public String toString() {
+
+        return nickname;
+    }
+}
